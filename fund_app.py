eb3cbab512cadf23304959835231b4df8f976320
irishg50 <95836644+irishg50@users.noreply.github.com>
2023-05-05 19:44:32 -0400

    update chat request db model

--- fund_app.py
+++ fund_app.py
@@ -67,6 +67,7 @@ class ChatRequest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     prompt = db.Column(db.Text, nullable=False)
+    topic = db.Column(db.String(250), nullable=True)
     engine = db.Column(db.String(50), nullable=False)
     chatgpt_response = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
