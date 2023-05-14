f35c9273d2322f95071fe34e5e42c5ad0c664734
irishg50 <95836644+irishg50@users.noreply.github.com>
2023-05-14 15:52:53 -0400

    remove pending message

--- fund_app.py
+++ fund_app.py
@@ -428,10 +428,8 @@ def taskstatus():
     task_id = session.get('task_id')
     task = send_request_to_chatgpt_task.AsyncResult(task_id)
     if task.state == 'PENDING':
-        response = {
-            'state': task.state,
-            'status': 'Pending...'
-        }
+
+        
     elif task.state == 'SUCCESS':
         response = {
             'state': task.state,
