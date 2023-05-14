1da6f57a502a401027efaf78dd6a951556949ff9
irishg50 <95836644+irishg50@users.noreply.github.com>
2023-05-14 13:42:47 -0400

    redis fix 3

--- fund_app.py
+++ fund_app.py
@@ -47,10 +47,8 @@ def make_celery(app):
 
 app = Flask(__name__)
 app.config.update(
-#    CELERY_BROKER_URL='rediss://:p952ada0b5ae194c7c49dd484e19814e03c9a324296ecfcfe8ff1ae4aca4ebc2e@ec2-3-234-14-83.compute-1.amazonaws.com:14850?ssl_cert_reqs=CERT_REQUIRED',
-#    CELERY_RESULT_BACKEND='rediss://:p952ada0b5ae194c7c49dd484e19814e03c9a324296ecfcfe8ff1ae4aca4ebc2e@ec2-3-234-14-83.compute-1.amazonaws.com:14850?ssl_cert_reqs=CERT_REQUIRED'
-    CELERY_BROKER_URL=r,
-    CELERY_RESULT_BACKEND=r
+    CELERY_BROKER_URL='rediss://:p952ada0b5ae194c7c49dd484e19814e03c9a324296ecfcfe8ff1ae4aca4ebc2e@ec2-3-234-14-83.compute-1.amazonaws.com:14850?ssl_cert_reqs=CERT_REQUIRED',
+    CELERY_RESULT_BACKEND='rediss://:p952ada0b5ae194c7c49dd484e19814e03c9a324296ecfcfe8ff1ae4aca4ebc2e@ec2-3-234-14-83.compute-1.amazonaws.com:14850?ssl_cert_reqs=CERT_REQUIRED'
 )
 celery = make_celery(app)
 
