546aaa3a58f2f9579ed9ad872ff1556e0b865629
irishg50 <95836644+irishg50@users.noreply.github.com>
2023-05-15 06:40:57 -0400

    task ID track

--- fund_app.py
+++ fund_app.py
@@ -427,6 +427,7 @@ def admin():
 @login_required
 def taskstatus():
     task_id = session.get('task_id')
+    print(f"Task ID retrieved from session: {task_id}")
     task = send_request_to_chatgpt_task.AsyncResult(task_id)
     if task.state == 'PENDING':
         response = {
